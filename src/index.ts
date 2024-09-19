05ca26c9dd52e5f4aac3ee72a0755f69037cf766
Lajos Meszaros <m_lajos@hotmail.com>
2024-09-19 21:48:14 +0200

    feat: add parquette texture and create a simple test room with it

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { ArxMap, Settings, Vector3 } from 'arx-level-generator'
+import { loadRooms } from 'arx-level-generator/prefabs/rooms'
 
 const settings = new Settings()
 const map = new ArxMap()
 
 map.config.offset = new Vector3(6000, 0, 6000)
+
+const apartmentType13rdFloor = await loadRooms('./apartment-type-1.rooms', settings)
+apartmentType13rdFloor.forEach((room) => {
+  map.add(room, true)
+})
 
 map.finalize(settings)
 map.saveToDisk(settings)
